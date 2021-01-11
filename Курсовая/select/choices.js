5b4ebb47881208f925a91d8f67d9d858b82e4d5f
Mark-front <76992085+Mark-front@users.noreply.github.com>
2021-01-11 12:59:18 +0300

    курсовя

--- Курсовая/select/choices.js
+++ Курсовая/select/choices.js
@@ -1,15 +1,15 @@
-const defaultSelect = () => {
-	const element = document.querySelector('.default');
-  const choices = new Choices(element, {
-		searchEnabled: false,
-		noResultsText: 'Ничего не найдено'
+const filterDefault = () => {
+	const elements = document.querySelectorAll('.filter-select');
+	elements.forEach(el => {
+		const choices = new Choices(el, {
+			searchEnabled: false,
+			noResultsText: 'Ничего не найдено'
+		});
 	});
+	
+}
 
-	let ariaLabel = element.getAttribute('aria-label');
-	element.closest('.choices').setAttribute('aria-label', ariaLabel);
-};
-
-defaultSelect();
+filterDefault();
 
 const multiDefault = () => {
 	const elements = document.querySelectorAll('.multi-default');
@@ -24,15 +24,3 @@ const multiDefault = () => {
 
 multiDefault();
 
-const filterDefault = () => {
-	const elements = document.querySelectorAll('.filter-select');
-	elements.forEach(el => {
-		const choices = new Choices(el, {
-			searchEnabled: false,
-			noResultsText: 'Ничего не найдено'
-		});
-	});
-	
-}
-
-filterDefault();
