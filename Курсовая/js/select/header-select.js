15497b8941a356f0992fabf877ef990ba868eb28
Mark-front <76992085+Mark-front@users.noreply.github.com>
2021-07-27 17:12:03 +0300

    fixed bugs

--- Курсовая/js/select/header-select.js
+++ Курсовая/js/select/header-select.js
@@ -2,8 +2,8 @@ function selectOpen() {
   const headerElement = document.querySelectorAll('.art-directions-item-btn');
   headerElement.forEach(el => {
     el.addEventListener('click', () => {
-      if (el.parentElement.parentElement.querySelector('.hiding-list')) {
-        const list = el.parentElement.parentElement.querySelector('.hiding-list');
+      if (el.parentElement.querySelector('.hiding-list')) {
+        const list = el.parentElement.querySelector('.hiding-list');
         document.querySelectorAll('.visible-list').forEach(item => {
           item.classList.remove('visible-list');
           item.classList.add('hiding-list');
@@ -11,8 +11,8 @@ function selectOpen() {
         list.classList.remove('hiding-list');
         list.classList.add('visible-list');
       } else {
-        if (el.parentElement.parentElement.querySelector('.visible-list')) {
-          const list = el.parentElement.parentElement.querySelector('.visible-list');
+        if (el.parentElement.querySelector('.visible-list')) {
+          const list = el.parentElement.querySelector('.visible-list');
           list.classList.remove('visible-list');
           list.classList.add('hiding-list');
         }
