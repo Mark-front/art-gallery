2bf0c66cb3f94dcc88147b734aab1649617e1ca2
Mark-front <76992085+Mark-front@users.noreply.github.com>
2021-08-24 19:26:29 +0300

    fixed bugs

--- Курсовая/js/events-btn.js
+++ Курсовая/js/events-btn.js
@@ -1,9 +1,8 @@
-// document.querySelector(".events__btn").addEventListener("click", () => {
-//   document.querySelector(".events__list").classList.toggle("events__list--open");
-// });
-
 document.addEventListener('DOMContentLoaded', () => { // Структура страницы загружена
 
+  const list = document.querySelector(".events__list");
+  const heightList = list.querySelector('.event-card').clientHeight;
+  list.style.maxHeight = heightList + 50 + 'px';
   const smoothHeight = (itemSelector, buttonSelector, contentSelector) => { // объявляем основную функцию, которая принимает в качестве параметров селекторы элемента, кнопки внутри элемента и блока с контентом
 
     const items = document.querySelectorAll(itemSelector) // находим все элементы по переданному селектору в параметре itemSelector и записываем в константу items
@@ -16,11 +15,14 @@ document.addEventListener('DOMContentLoaded', () => { // Структура ст
 
       button.addEventListener('click', () => { // при клике на кнопку
         if (el.dataset.open !== 'true') { // если значение data-атрибута open у элемента не равно 'true' и блок с контентом еще не отображен
+          console.log(el.dataset, el.dataset.open)
           el.dataset.open = 'true' // тогда устанавливаем значение 'true'
           content.style.maxHeight = `${content.scrollHeight}px` // и блоку с контентом устанавливаем inline-свойсво max-height со вычисленным значением полной высоты этого блока
         } else { // если блок с контентом отображен и значение data-атрибута open у элемента равно 'true'
+          console.log(el.dataset, el.dataset.open)
           el.dataset.open = 'false' // тогда устанавливаем значение 'false'
-          content.style.maxHeight = '' // и сбрасываем ранее установленное inline-свойсво max-height
+          console.log(heightList)
+          content.style.maxHeight = heightList + 50 + 'px'; // и сбрасываем ранее установленное inline-свойсво max-height
         }
       })
 
