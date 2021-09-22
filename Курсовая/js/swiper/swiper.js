759e223ee457677d632a268e9fbf0d266acfd2cb
Никоноров Марк <marvanik@gmail.com>
2021-09-22 12:03:24 +0300

    fixed bugs

--- Курсовая/js/swiper/swiper.js
+++ Курсовая/js/swiper/swiper.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event)
 {
   if ( window.matchMedia("(max-width: 1024px)").matches) {
     document.querySelector(".swiper-1920").remove();
-  }
+  } else document.querySelector(".swiper-1024").remove();
   var mySwiper = new Swiper('.gallery__swiper', {
     // Optional parameters
     direction: 'horizontal',
